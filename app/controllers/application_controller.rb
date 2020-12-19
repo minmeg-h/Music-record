82f4ebaa3505876f51fae534ab335376d8df7f5e
Minmeg <minmeg995@gmail.com>
2020-12-19 19:02:30 +0900

    application_controllerの変更#35

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    homes_index_path
+    #user_path(resource)
+end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
