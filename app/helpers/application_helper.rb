20fd3fea89bd19af2850d1196c0e306ad88a7843
Minmeg <minmeg995@gmail.com>
2020-12-24 00:01:19 +0900

    画像のURLの変更4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def profile_photo(user)
     return user.profile_photo unless user.profile_photo.nil?
 
-    user.profile_photo = '/assets/default_icon.jpeg'
+    user.profile_photo = 'default_icon.jpeg'
   end
 end
