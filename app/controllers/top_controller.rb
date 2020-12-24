e0d490f9bb22d56fdaac1ba23a365331d02e2bcd
Minmeg <minmeg995@gmail.com>
2020-12-24 22:52:47 +0900

    rubocopの修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,6 @@
 class TopController < ApplicationController
-  def top;
-    flash[:notice] = "ログインしています" if user_signed_in?
-    redirect_to  user_path(current_user)
+  def top
+    flash[:notice] = 'ログインしています' if user_signed_in?
+    redirect_to user_path(current_user)
   end
-
 end
