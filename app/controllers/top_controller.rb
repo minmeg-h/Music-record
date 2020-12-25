cac73a9759f9f51566a2a5ccff00dc464bf76d29
Minmeg <minmeg995@gmail.com>
2020-12-25 21:40:54 +0900

    TOPページにログインユーザーのアクセスを制限#63

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,3 +1,15 @@
 class TopController < ApplicationController
+  before_action :redirect_profile
   def top; end
+
+
+private
+  def redirect_profile
+    if user_signed_in?
+      flash[:notice] = 'ログインしています'
+      redirect_to homes_index_path
+    end
+
+  end
+
 end
