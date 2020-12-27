b25343fd9ba6b75b1d3848ae34e2ffc2173e0022
Minmeg <minmeg995@gmail.com>
2020-12-27 22:46:12 +0900

    見た目の変更#54

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
- protected
- def update_resource(resource, params)
-   resource.update_without_current_password(params)
- end
+  protected
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
+  end
 end
