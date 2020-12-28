8e4b886d32efd63acb8a111d20eb708101559497
Minmeg <minmeg995@gmail.com>
2020-12-28 12:04:05 +0900

    コントローラーが重複していたので整理#54

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,44 +1,19 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+   protected
+   # パスワードなしでユーザー情報の変更ができる
+   def update_resource(resource, params)
+     resource.update_without_current_password(params)
+   end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
+   # ユーザー情報変更後のリダイレクト先を指定
+   def after_update_path_for(resource)
+     user_path(resource)
+   end
+ end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -59,4 +34,3 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-end
