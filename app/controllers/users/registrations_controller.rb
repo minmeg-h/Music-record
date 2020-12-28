55575ad053f56b91a3bcbaaf02feba8f8111f704
Minmeg <minmeg995@gmail.com>
2020-12-28 12:30:33 +0900

    rubocopの修正1#54

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,36 +1,13 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
+  protected
 
+  # パスワードなしでユーザー情報の変更ができる
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
+  end
 
-   protected
-   # パスワードなしでユーザー情報の変更ができる
-   def update_resource(resource, params)
-     resource.update_without_current_password(params)
-   end
-
-   # ユーザー情報変更後のリダイレクト先を指定
-   def after_update_path_for(resource)
-     user_path(resource)
-   end
- end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  # ユーザー情報変更後のリダイレクト先を指定
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
   validates :profile, length: { maximum: 200 }
 
   def update_without_current_password(params, *options)
-   params.delete(:current_password)
+    params.delete(:current_password)
 
    if params[:password].blank? && params[:password_confirmation]
      params.delete(:password)
      params.delete(:password_confirmation)
    end
-
-   result = update_attributes(params, *options)
-   clean_up_passwords
-   result
- end
+    # result = update_attributes(params, *options)
+    result = update_attributes(params, *options)
+     clean_up_passwords
+      result
+  end
 end
