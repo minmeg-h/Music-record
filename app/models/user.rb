116ec0a1828c783f4f82eccaf471ee8dab87b141
Minmeg <minmeg995@gmail.com>
2021-01-03 01:06:47 +0900

    ユーザー新規登録のテストを作成#72

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,6 @@ class User < ApplicationRecord
   def password_complexity
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,70}$/
 
-    errors.add :password, 'パスワードの強度が不足しています。パスワードの長さは8〜70文字とし、大文字と小文字と数をそれぞれ1文字以上含める必要があります。'
+    errors.add :password, 'パスワードの強度が不足しています。パスワードの長さは8文字以上とし、大文字と小文字と数をそれぞれ1文字以上含める必要があります。'
   end
 end
