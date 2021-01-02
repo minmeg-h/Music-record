15909664d8ad7738264fc63160a7b21d07376c92
Minmeg <minmeg995@gmail.com>
2021-01-02 23:59:01 +0900

    userのテストデータの準備#72

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
+  # fakerの導入を検討する
   factory :user do
+    email                 {"test@gmail.com"}
+    name                  {"テスト"}
+    password              {"111111"}
+    password_confirmation {"111111"}
   end
 end
