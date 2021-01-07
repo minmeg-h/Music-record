1f9c457ef9b856d520f4baca8ec3260be6b3abd8
Minmeg <minmeg995@gmail.com>
2021-01-07 21:13:13 +0900

    テータ型の変更 #76

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def profile_photo(user)
-    return user.profile_photo unless user.profile_photo.nil?
-
+    return user.profile_photo unless user.profile_photo.blank?
     user.profile_photo = 'default_icon.jpg'
   end
 end
