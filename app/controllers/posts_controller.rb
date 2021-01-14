7d440b3510ee8cec4b6646f926a90d14c8b693a9
Minmeg <minmeg995@gmail.com>
2021-01-15 01:13:15 +0900

    newとcreateアクションを追加#45

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+    #@post.music.build
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:text)
+    end
+
 end
