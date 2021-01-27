0766738f69689481e2ca3a453b0dc2a96df6817c
Minmeg <minmeg995@gmail.com>
2021-01-28 01:36:42 +0900

    投稿編集機能の実装 #84

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post, only: %i[show destroy]
+  before_action :set_post, only: %i[show  edit update destroy]
 
   def new
     @post = Post.new
@@ -27,9 +27,9 @@ class PostsController < ApplicationController
 
   def update
     if @post.user == current_user
-      flash[:notice] = '投稿が編集されました' if @post.update_attributes(post_params)
+      flash[:notice] = '投稿を編集しました' if @post.update(post_params)
     else
-      flash[:notice] = '投稿の編集に失敗しました'
+      flash[:notice] = '投稿の修正に失敗しました'
     end
     redirect_to posts_path
   end
@@ -45,10 +45,12 @@ class PostsController < ApplicationController
 
   private
 
+ # ストロングパラメーターを設定（投稿には文字文章にログインユーザーのidを付与する）
   def post_params
     params.require(:post).permit(:text).merge(user_id: current_user.id)
   end
 
+ # リファクタリングのためにあらかじめ、対象の投稿を探しておくメソッド
   def set_post
     @post = Post.find_by(id: params[:id])
   end
