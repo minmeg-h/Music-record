e7dc8853f3130f5281224951806e5fe310dcc3f4
Minmeg <minmeg995@gmail.com>
2021-03-07 23:30:49 +0900

    rubocopの指摘の修正

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -3,9 +3,6 @@ class MusicsController < ApplicationController
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
 
   def search
-    if params[:search].present?
-    @musics = RSpotify::Track.search(params[:search])
-    artists = @musics.first.artists
-    end
+    @musics = RSpotify::Track.search(params[:search]) if params[:search].present?
   end
 end
